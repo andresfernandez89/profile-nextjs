5c64bf99a7ccbe28c90d5d75e507259c1a6e6b97
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-07-04 18:38:11 -0300

    style: Styled navbar and managed states

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,21 +1,49 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 import styles from "../styles/navbar.module.css";
 import ThemeSwitcher from "./themeSwitcher";
 
 const Navbar = () => {
+  const [activeSection, setActiveSection] = useState("");
+
+  function handleClck(props: string): void {
+    setActiveSection(props);
+  }
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li>
-          <a href="#about">Sobre mí</a>
-        </li>
-        <li>
-          <a href="#interests">Intereses</a>
-        </li>
-        <li>
-          <a href="#contact">Contacto</a>
-        </li>
+        <div>
+          <li>
+            <Link
+              onClick={() => handleClck("about")}
+              href="#about"
+              className={activeSection === "about" ? styles.active : ""}
+            >
+              Sobre mí
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => handleClck("interests")}
+              href="#interests"
+              className={activeSection === "interests" ? styles.active : ""}
+            >
+              Intereses
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => handleClck("contact")}
+              href="#contact"
+              className={activeSection === "contact" ? styles.active : ""}
+            >
+              Contacto
+            </Link>
+          </li>
+        </div>
+        <ThemeSwitcher />
       </ul>
-      <ThemeSwitcher />
     </nav>
   );
 };
