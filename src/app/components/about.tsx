02391802a3905db0dfb61ec89bd40f4fa9ab9b1f
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-07-05 12:53:31 -0300

    feat: Add content to About section

---
+++ src/app/components/about.tsx
@@ -0,0 +1,38 @@
+import ManIcon from "@/icons/manIcon";
+import styles from "../styles/about.module.css";
+
+const About = () => {
+  return (
+    <section className={styles.about}>
+      <h2 className={styles.myHeading}>
+        <ManIcon className={styles.manIcon} />
+        Sobre mí
+      </h2>
+      <p>
+        Soy un Frontend Developer dedicado que durante los últimos 4 años ha
+        perfeccionado tanto habilidades técnicas como soft skills. He colaborado
+        activamente en equipos comprometidos, desarrollando proyectos bajo
+        metodologías Agile y siguiendo las mejores prácticas.
+      </p>
+      <p>
+        Mi principal objetivo es contribuir a la mejora continua de los procesos
+        en los que me involucro, siempre en busca de oportunidades de
+        crecimiento profesional. Soy una persona emprendedora, con gran
+        curiosidad y una mentalidad orientada a superar mis propios límites.
+        Asumo los errores como oportunidades de aprendizaje y me esfuerzo por
+        mejorar constantemente.
+      </p>
+      <p>
+        Actualmente, busco un entorno laboral que fomente el crecimiento, el
+        aprendizaje y la innovación, donde pueda contribuir activamente y
+        enfrentar nuevos retos.
+      </p>
+      <p>
+        Disfruto especialmente construyendo proyectos innovadores en equipo,
+        facilitando la colaboración y la reflexión a través de retrospectivas.
+      </p>
+    </section>
+  );
+};
+
+export default About;
